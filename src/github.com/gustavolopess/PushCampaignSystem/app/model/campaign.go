6d8abe4d63c0a33afbc7ddf42763f554cd51ec03
Gustavo Lopes <gustavo.hlopess@gmail.com>
2019-08-10 02:26:12 -0300

    [01] methods to process campaigns

--- src/github.com/gustavolopess/PushCampaignSystem/app/model/campaign.go
+++ src/github.com/gustavolopess/PushCampaignSystem/app/model/campaign.go
@@ -1,9 +1,30 @@
 package model
 
+import (
+	"encoding/json"
+	"io/ioutil"
+)
+
 // model campaign
 type Campaign struct {
 	ID          int64  	`json:"id"`
 	Provider    string 	`json:"provider"`
 	PushMessage string 	`json:"push_message"`
 	Targeting	[]Place	`json:"targeting"`
+}
+
+
+func LoadCampaigns(filePath string) (campaigns []Campaign, err error) {
+	fileData, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return
+	}
+
+	err = json.Unmarshal(fileData, &campaigns)
+	return
+}
+
+
+func (c *Campaign) Store() {
+
 }
