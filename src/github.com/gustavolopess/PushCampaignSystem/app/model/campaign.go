811a7b645c2fb6a3171f1a61dc421a7204165e8b
Gustavo Lopes <gustavo.hlopess@gmail.com>
2019-08-10 15:13:11 -0300

    [04] store being done with upsert and set mongo contexts to background with timeout

--- src/github.com/gustavolopess/PushCampaignSystem/app/model/campaign.go
+++ src/github.com/gustavolopess/PushCampaignSystem/app/model/campaign.go
@@ -4,12 +4,15 @@ import (
 	"context"
 	"encoding/json"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"io/ioutil"
+	"time"
 )
 
 // model campaign
 type Campaign struct {
-	ID          int64  	`json:"id"`
+	ID          int64  	`json:"id" bson:"_id"`
 	Provider    string 	`json:"provider"`
 	PushMessage string 	`json:"push_message"`
 	Targeting	[]Place	`json:"targeting"`
@@ -28,19 +31,49 @@ func LoadCampaigns(filePath string) (campaigns []Campaign, err error) {
 }
 
 // Store campaign on database
-func (c *Campaign) Store() (err error) {
-	_, err = MongoCollection().InsertOne(context.TODO(), *c)
+func (c *Campaign) StoreOne(mongoCollection *mongo.Collection) (err error) {
+	// Uses upsert to update if it exists and create if doesn't and avoid existent key error
+	updateOptions := &options.UpdateOptions{}
+	updateOptions = updateOptions.SetUpsert(true)
+
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	_, err = mongoCollection.UpdateOne(
+		ctx,
+		bson.M{"_id": c.ID},
+		bson.D{
+			{"$set", bson.D{
+				{"provider", c.Provider},
+				{"push_message", c.PushMessage},
+				{"targeting", c.Targeting},
+			}},
+	}, updateOptions)
 	return
 }
 
 // Store multiple campaigns
-func StoreMultipleCampaigns(campaigns []Campaign) (err error) {
-	campaignsToStore := make([]interface{}, len(campaigns))
-	for i, c := range campaigns {
-		campaignsToStore[i] = c
+func StoreMultipleCampaigns(campaigns []Campaign, mongoCollection *mongo.Collection) (err error) {
+	var operations []mongo.WriteModel
+
+	// Uses upsert to update if it exists and create if doesn't and avoid existent key error
+	for _, c := range campaigns {
+		operation := mongo.NewUpdateOneModel()
+		operation.SetFilter(bson.M{"_id": c.ID})
+		operation.SetUpdate(bson.D{
+			{"$set", bson.D{
+				{"provider", c.Provider},
+				{"push_message", c.PushMessage},
+				{"targeting", c.Targeting},
+			}},
+		})
+		operation.SetUpsert(true)
+
+		operations = append(operations, operation)
 	}
 
-	_, err = MongoCollection().InsertMany(context.TODO(), campaignsToStore)
+	// Bulk write to mitigate lacks of performance
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	_, err = mongoCollection.BulkWrite(ctx, operations)
+
 	return
 }
 
@@ -48,13 +81,16 @@ func StoreMultipleCampaigns(campaigns []Campaign) (err error) {
 // Search campaigns by visit/targeting
 func SearchCampaignsByVisit(visit Visit) (results []*Campaign, err error) {
 
-	cursor, err := MongoCollection().Find(context.TODO(), bson.M{"targeting.place_id": visit.PlaceId})
+	// Search campaigns which contains the visit's place into its targeting
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	cursor, err := MongoCollection().Find(ctx, bson.M{"targeting.place_id": visit.PlaceId})
 	if err != nil {
 		return
 	}
+	defer cursor.Close(ctx)
 
-
-	for cursor.Next(context.TODO()) {
+	// Decode results into array of campaigns
+	for cursor.Next(ctx) {
 		// object to receive decoded document
 		var campaign Campaign
 		err = cursor.Decode(&campaign)
