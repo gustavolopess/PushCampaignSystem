200f38502637a6edff18405ee944852f1c6c3bfa
Gustavo Lopes <gustavo.hlopess@gmail.com>
2019-08-11 00:23:37 -0300

    [09] created controller to deal with visits

--- src/github.com/gustavolopess/PushCampaignSystem/app/controller/visit.go
+++ src/github.com/gustavolopess/PushCampaignSystem/app/controller/visit.go
@@ -1,1 +1,45 @@
 package controller
+
+import (
+	"github.com/gustavolopess/PushCampaignSystem/app/model"
+	"github.com/hpcloud/tail"
+	"log"
+)
+
+// Return generator to lines in log file
+func TailVisitLogFile(filePath string) chan *tail.Line {
+	t, err := tail.TailFile(filePath, tail.Config{Follow: true})
+	if err != nil {
+		log.Fatalf("Could not tail visit log file: %s", err.Error())
+	}
+
+	return t.Lines
+}
+
+// Search campaigns which contain same place_id as present in log line
+func SearchCampaignsByLogLine(line string) (*model.Visit, []*model.Campaign) {
+	// Parse line to visit
+	visit, err := model.ParseVisitLogLine(line)
+	if err != nil {
+		log.Fatalf("Could not parse line '%s': %s", line, err.Error())
+	}
+
+	campaigns, err := model.SearchCampaignsByVisit(visit)
+	if err != nil {
+		log.Fatalf("Could not search campaign with visit %#v: %s", visit, err.Error())
+	}
+
+	return visit, campaigns
+}
+
+// Insert message into NATS pub queue
+func EnqueueMessageIntoNats(natsMessage *model.NatsMessage, natsConn *model.NatsConn) {
+	_, err := natsMessage.EnqueueIntoNats(natsConn)
+	if err != nil {
+		log.Fatalf("Could not enqueue message %#v into NATS streaming: %s", natsMessage, err.Error())
+	}
+
+	log.Printf("Message %#v successfuly enqueued into NATS streaming\n", natsMessage)
+}
+
+
--- src/github.com/gustavolopess/PushCampaignSystem/cmd/pub/main.go
+++ src/github.com/gustavolopess/PushCampaignSystem/cmd/pub/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"github.com/gustavolopess/PushCampaignSystem/app/controller"
 	"github.com/gustavolopess/PushCampaignSystem/app/model"
 	"os"
 	"os/signal"
@@ -14,11 +15,42 @@ func main() {
 		"etc/queue.json",
 		"Path to file with NATS configuration")
 
+	mongoConfigPath := flag.String(
+		"mongoconfig",
+		"etc/mongoConfig.json",
+		"Path to file with MongoDB configuration")
+
+	visitLogPath := flag.String(
+		"visitlogpath",
+		"input/visit.log",
+		"Path to log with visits which will be tailed")
+
 	flag.Parse()
 
 	// Init NATS configuration instance
 	var natsConn model.NatsConn
 	natsConn.LoadConfig(*natsConfigPath)
+	natsConn.Connect(model.PubQueue)
+
+	// init MongoDB
+	var mongoConn model.MongoConn
+	mongoConn.LoadConfig(*mongoConfigPath)
+	mongoConn.Connect()
+
+	logFileTail := controller.TailVisitLogFile(*visitLogPath)
+	for line := range logFileTail {
+		go func() {
+			visit, campaigns := controller.SearchCampaignsByLogLine(line.Text)
+			for _, c := range campaigns {
+				natsMessage := &model.NatsMessage{
+					Provider: c.Provider,
+					Message:  c.PushMessage,
+					DeviceId: visit.DeviceId,
+				}
+				go controller.EnqueueMessageIntoNats(natsMessage, &natsConn)
+			}
+		}()
+	}
 
 
 	// Subscribe to SIGINT signals
