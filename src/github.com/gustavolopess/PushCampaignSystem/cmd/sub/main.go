fd88318735a07c2837190726256f66a8b6e8e507
Gustavo Lopes <gustavo.hlopess@gmail.com>
2019-08-11 16:47:27 -0300

    [12] implement subscriber daemon

--- src/github.com/gustavolopess/PushCampaignSystem/cmd/sub/main.go
+++ src/github.com/gustavolopess/PushCampaignSystem/cmd/sub/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"github.com/gustavolopess/PushCampaignSystem/app/controller"
+	"github.com/gustavolopess/PushCampaignSystem/app/model"
+	"os"
+	"os/signal"
+)
 
 func main() {
-	fmt.Print("SUB")
+	// Command line flags
+	natsConfigPath := flag.String(
+		"natsconfig",
+		"etc/queue.json",
+		"Path to file with NATS configuration")
+	flag.Parse()
+
+	// Init NATS configuration instance and connect to streaming server
+	var natsConn model.NatsConn
+	natsConn.LoadConfig(*natsConfigPath)
+	natsConn.Connect(model.SubQeueue)
+
+	// Subscribe to NATS messages
+	controller.DequeueMessagesFromNats(&natsConn)
+
+	// Subscribe to SIGINT signals
+	interruptChan := make(chan os.Signal)
+	signal.Notify(interruptChan, os.Interrupt)
+
+
+	// Wait for SIGINT
+	<- interruptChan
 }
